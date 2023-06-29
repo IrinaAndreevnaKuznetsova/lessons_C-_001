2b1e64e855f78f47c9f6229ebf58d6721c773212
irina_pr2020@mail.ru <irinakuznecova@MacBook-Air-Irina.local>
2023-06-29 11:27:53 +0300

    Created a file max

---
+++ Example_6_max/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int a = 3;
+int b = 4;
+int c = 9;
+int d = 2;
+int e = 10;
+
+int max = a;
+
+if (b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.WriteLine(max);
