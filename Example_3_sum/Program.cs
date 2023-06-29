feb0f965735da4c03232d2fcd50950b941aa3d71
irina_pr2020@mail.ru <irinakuznecova@MacBook-Air-Irina.local>
2023-06-29 11:10:50 +0300

    finalized the fail sum

--- Example_3_sum/Program.cs
+++ Example_3_sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿int numberA = new Random() .Next(1,10);
+Console.WriteLine("Первое число: " + numberA);
+int numberB = new Random() .Next(1,10);
+Console.WriteLine("Второе число: " + numberB);
+int result = (numberA + numberB);
+Console.WriteLine("Сумма двух чисел: " + result);
